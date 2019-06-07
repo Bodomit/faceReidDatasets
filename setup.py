0d7f9c2b2d545c4f20707a128b0adacb5ce227be
Glen Brown <gbrown29@qub.ac.uk>
2019-06-07 14:33:04 +0100

    Added requirements file.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='FaceReidDatasets',
     version='0.1dev',
-    packages=['faceReidDatasets',],
+    packages=['faceReidDatasets'],
     license='GNU Affero General Public License v3.0',
     long_description=open('README.md').read(),
+    install_requires=requirements
 )
