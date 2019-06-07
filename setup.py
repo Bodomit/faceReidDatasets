2bfd16371580e4b3b4a4db0905642fd7f6d41e0e
Glen Brown <gbrown29@qub.ac.uk>
2019-06-07 14:02:25 +0100

    Initial Commit: setup and readme.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='FaceReidDatasets',
+    version='0.1dev',
+    packages=['faceReidDatasets',],
+    license='GNU Affero General Public License v3.0',
+    long_description=open('README.md').read(),
+)
