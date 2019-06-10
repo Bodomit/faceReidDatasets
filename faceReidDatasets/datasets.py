c3ba1fde2cdce72f6386e8d269bbcb671c83d36b
Glen Brown <gbrown29@qub.ac.uk>
2019-06-10 12:31:59 +0100

    Fixed DatasetBase according to unit tests.

--- faceReidDatasets/datasets.py
+++ faceReidDatasets/datasets.py
@@ -14,14 +14,17 @@ class DatasetBase(abc.ABC):
     """
 
     def __init__(self, dataset: List[Set[Tuple[str, str]]]):
-        self._dataset = sorted(dataset, key=lambda x: x[1])
+        self._dataset = sorted(dataset, key=lambda x: (x[1], x[0]))
 
     @property
     def dataset(self):
         return self._dataset
 
     def __iter__(self):
-        return self.dataset.__iter__
+        return self.dataset.__iter__()
 
     def as_target_to_source_list(self):
-        return dict(itertools.groupby(self._dataset, lambda x: x[1]))
+        target_to_source_list = {}
+        for k, g in itertools.groupby(self._dataset, lambda x: x[1]):
+            target_to_source_list[k] = [i[0] for i in g]
+        return target_to_source_list
