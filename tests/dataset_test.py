8455d7ea7d8ae627daa271d3a3d8cd3b882383b5
Glen Brown <gbrown29@qub.ac.uk>
2019-06-10 12:28:46 +0100

    Added unit tests for DatasetBase.

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -1,5 +1,5 @@
 import unittest
-import faceReidDatasets
+from faceReidDatasets import datasets
 
 
 class DatasetBaseBasic(unittest.TestCase):
@@ -13,5 +13,27 @@ class DatasetBaseBasic(unittest.TestCase):
         ]
 
     def test_init(self):
-        dataset = faceReidDatasets.datasets.DatasetBase(self.data)
+        dataset = datasets.DatasetBase(self.data)
         self.assertIsNotNone(dataset)
+        sorted_dataset = [("A", 1),
+                          ("D", 1),
+                          ("B", 2),
+                          ("E", 2),
+                          ("C", 3)]
+        self.assertListEqual(dataset.dataset, sorted_dataset)
+
+    def test_iterator(self):
+        dataset = datasets.DatasetBase(self.data)
+        sorted_dataset = [("A", 1),
+                          ("D", 1),
+                          ("B", 2),
+                          ("E", 2),
+                          ("C", 3)]
+        self.assertListEqual(list(dataset), sorted_dataset)
+
+    def test_as_target_to_source_list(self):
+        dataset = datasets.DatasetBase(self.data).as_target_to_source_list()
+        correct = {1: ["A", "D"],
+                   2: ["B", "E"],
+                   3: ["C"]}
+        self.assertDictEqual(dataset, correct)
