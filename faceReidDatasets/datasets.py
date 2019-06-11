72ff551a877c04a5afa61404df9794d5dcfe8b1e
Glen Brown <gbrown29@qub.ac.uk>
2019-06-11 09:32:48 +0100

    Added multilevel dataset with tests.

--- faceReidDatasets/datasets.py
+++ faceReidDatasets/datasets.py
@@ -28,3 +28,38 @@ class DatasetBase(abc.ABC):
         for k, g in itertools.groupby(self._dataset, lambda x: x[1]):
             target_to_source_list[k] = [i[0] for i in g]
         return target_to_source_list
+
+
+class MutiLevelDatasetBase(abc.ABC):
+    """
+    #TODO
+    """
+
+    def __init__(self, dataset: dict):
+        self._dataset = self._populate_dataset(dataset)
+
+    def _traverse(self, node, f):
+        if isinstance(node, list) or isinstance(node, DatasetBase):
+            return f(node)
+        elif isinstance(node, dict):
+            return {k: self._traverse(v, f) for k, v in node.items()}
+        else:
+            raise ValueError("Node must be a list of tuples.")
+
+    def _populate_dataset(self, dataset):
+        return self._traverse(dataset, lambda d: DatasetBase(d))
+
+    @property
+    def dataset(self):
+        return self._dataset
+
+    def __iter__(self):
+        return self.dataset.__iter__()
+
+    def __getitem__(self, key):
+        return self.dataset[key]
+
+    def as_target_to_source_list(self):
+        return self._traverse(self.dataset, 
+                              lambda d: d.as_target_to_source_list())
+    
--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -37,3 +37,51 @@ class DatasetBaseBasic(unittest.TestCase):
                    2: ["B", "E"],
                    3: ["C"]}
         self.assertDictEqual(dataset, correct)
+
+
+class MultiLevelDatasetBaseBasic(unittest.TestCase):
+    def setUp(self):
+        self.data = {
+            "train": [("A", 1),
+                      ("B", 2),
+                      ("C", 3),
+                      ("D", 1),
+                      ("E", 2)],
+            "test":  [("F", 10),
+                      ("G", 11),
+                      ("H", 10)]
+        }
+
+    def test_init(self):
+        dataset = datasets.MutiLevelDatasetBase(self.data)
+        self.assertIsNotNone(dataset)
+        sorted_dataset = {
+            "train": [("A", 1),
+                      ("D", 1),
+                      ("B", 2),
+                      ("E", 2),
+                      ("C", 3)],
+            "test":  [("F", 10),
+                      ("H", 10),
+                      ("G", 11)]}
+        self.assertListEqual(dataset.dataset["train"].dataset,
+                             sorted_dataset["train"])
+        self.assertListEqual(dataset.dataset["test"].dataset,
+                             sorted_dataset["test"])
+
+    def test_iterator(self):
+        dataset = datasets.MutiLevelDatasetBase(self.data)
+        self.assertListEqual(sorted(list(dataset)), sorted(["train", "test"]))
+
+    def test_as_target_to_source_list(self):
+        dataset = datasets.MutiLevelDatasetBase(self.data)
+        dataset = dataset.as_target_to_source_list()
+        correct = {
+            "train": {1: ["A", "D"],
+                      2: ["B", "E"],
+                      3: ["C"]},
+
+            "test":  {10: ["F", "H"],
+                      11: ["G"]}
+        }
+        self.assertDictEqual(dataset, correct)
