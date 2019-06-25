9d63ece47946085af89140339c1be89da700e390
Glen Brown <gbrown29@qub.ac.uk>
2019-06-25 09:41:39 +0100

    Typos in package name.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='friedds',
+    name='freidds',
     version='0.1dev',
-    packages=['friedds'],
+    packages=['freidds'],
     license='GNU Affero General Public License v3.0',
     long_description=open('README.md').read(),
     install_requires=requirements
